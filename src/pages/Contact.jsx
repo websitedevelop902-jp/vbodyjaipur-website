b223b91928508d2281d0d93803d89eea6806dec7
Anjali Jangid <anjaljangid2003@gmail.com>
2026-06-29 19:35:34 +0530

    Updated favicon and title

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -85,13 +85,18 @@ function Contact() {
 
           </div>
 
-          <div className="map-section">
+          <div className="contact-map">
+            <h2>Our Location</h2>
+
             <iframe
-              title="location"
-              src="https://maps.google.com/maps?q=Jaipur&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3597.1201106608733!2d75.80398607125711!3d27.045291616909953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1782741109159!5m2!1sen!2sin"
               width="100%"
-              height="350"
-              style={{ border: 0 }}
+              height="400"
+              style={{ border: 0, borderRadius: "10px" }}
+              loading="lazy"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              title="Google Map"
             ></iframe>
           </div>
 
