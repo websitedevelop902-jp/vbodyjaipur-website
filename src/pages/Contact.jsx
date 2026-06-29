7970b0a3b39a19ec8068e83fedc41a75582379bc
Anjali Jangid <anjaljangid2003@gmail.com>
2026-06-29 22:49:18 +0530

    Updated favicon and title

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,6 +22,7 @@ function Contact() {
                   +91 9660244357
                 </a>
               </p>
+
               📧{" "}
               <a
                 href="https://mail.google.com/mail/?view=cm&fs=1&to=vishwakarma.busbody@gmail.com"
@@ -31,14 +32,16 @@ function Contact() {
                 vishwakarma.busbody@gmail.com
               </a>
 
-              <a
-                href="https://www.google.com/maps/dir/?api=1&destination=27.046062,75.806185"
-                className="btn btn-warning"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                📍 Get Directions
-              </a>
+              <p>
+                <a
+                  href="https://www.google.com/maps/dir/?api=1&destination=27.046062,75.806185"
+                  className="btn btn-warning"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  📍 Get Directions
+                </a>
+              </p>
 
               <p>🕒 Mon - Sat : 9:00 AM - 7:00 PM</p>
 
@@ -92,15 +95,15 @@ function Contact() {
             <h2>Our Location</h2>
 
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3597.1201106608733!2d75.80398607125711!3d27.045291616909953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1782741109159!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3553.492411047689!2d75.80446447381982!3d27.046200876567877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396daf00492ae0f5%3A0xade8cd6de6a6d98e!2sAnand%20pur!5e0!3m2!1sen!2sin!4v1782753369984!5m2!1sen!2sin"
               width="100%"
-              height="400"
+              height="450"
               style={{ border: 0, borderRadius: "10px" }}
               loading="lazy"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
               title="Google Map"
-            ></iframe>
+            />
           </div>
 
         </div>
